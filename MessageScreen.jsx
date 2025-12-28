6756614cd45d696f5fc9b81ceb6e6dbed71bfb12
gouravbhakt5-hue <gouravbhakt5@gmail.com>
2025-12-29 02:52:40 +0530

    Update birthday message for Aashi

--- MessageScreen.jsx
+++ MessageScreen.jsx
@@ -27,13 +27,15 @@ export default function MessageScreen({ onNext }) {
                     className=" h-auto max-w-xl bg-gradient-to-br from-pink-200 via-pink-100 to-pink-50 rounded-2xl shadow-lg p-4 md:p-6 text-center"
                 >
                     <p className="text-[#301733] text-base md:text-lg leading-relaxed overflow-y-auto max-h-[400px] pr-2">
-                        Happy Birthday, Cutiepie! You deserve all the happiness, love, and smiles in the world today and always.
-                        You have this special way of making everything around you brighter, your smile, your kindness, and the way
-                        you make people feel truly cared for. I hope your day is filled with laughter, surprises, and moments that
-                        make your heart happy. You’re truly one of a kind, and I just want you to know how special you are.
-                        Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness,
-                        success, and all the sweet things life has to offer. 💗
-                        Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness,
+                        Happy Birthday, Cutiepie! Hey Aashi,
+                        I wanted to make your birthday special, so I’m writing this for you.
+                        Socha nahi tha na ki apan itne achhe dost ban jaayenge? Apni friendship unexpected thi, but honestly bahut special hai.
+                        In these 4 years, I’ve learned that real friendship loud nahi hoti, bas consistent hoti hai—and tu hamesha wahi rahi hai, har situation mein mere saath.
+                        Tu genuinely bahut achhi hai—cute si, caring, aur teri smile toh best hai, jo kisi ka bhi mood achha kar de. Tu hamesha meri help karti hai and listens to me, and that makes me feel really important.
+                        Thank you for being the same kind and real Aashi I met years ago.
+                        I’m really grateful to have you in my life.
+                        Happy Birthday, Aashi 🎂💛
+                        Stay happy, stay healthy, and keep smiling always ✨
                     </p>
                 </motion.div>
             </div>
